93f3090a749cd9b30b367ad06a549ea5016704bc
straychild01 <straychild01@gmail.com>
2019-03-12 10:24:22 -0600

    Actualizada lista negra 2019-03-12

--- updateBlackList.sh
+++ updateBlackList.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-sudo cp $HOME/rancheroCall/ncidd.blacklist /etc/ncid
+RANCHEROCALL_DIR="$HOME/rancheroCall"
+
+cd $RANCHEROCALL_DIR
+sudo cp $RANCHEROCALL_DIR/ncidd.blacklist /etc/ncid
 sudo systemctl restart ncidd
 git add --all
 git commit -m "Actualizada lista negra $(date +%Y-%m-%d)"
 git push origin master
+cd -
